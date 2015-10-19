adcc5eb599716cfe4d7a2dba6ccf28da4f8d0512
mycrobe <jmulvane@cshl.edu>
2015-10-19 16:09:17 -0400

    Serve documentation for users going to root, and expose swagger as JSON.

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 'use strict';
 
 var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var yaml = require('js-yaml');
 
 module.exports = app;
 
 var config = {
   appRoot: __dirname // required config
 };
+
+var schema = yaml.load(fs.readFileSync(__dirname + '/api/swagger/swagger.yaml', 'utf8'));
 
 SwaggerExpress.create(config, function (err, swaggerExpress) {
   if (err) { throw err; }
@@ -17,5 +22,19 @@ SwaggerExpress.create(config, function (err, swaggerExpress) {
 
   var port = process.env.PORT || 10010;
   app.listen(port);
+
+  console.log('Listening on', port);
+
+  // define routes
+  app.get('/', function (req, res, next) { // return top level info
+    res.redirect('/docs?url=/gramene.json');
+  });
+
+  app.use('/docs', express.static('node_modules/swagger-ui/dist'));
+
+  app.get('/gramene.json', function (req, res, next) { // return top level info
+    res.json(schema);
+  });
+
 });
 
