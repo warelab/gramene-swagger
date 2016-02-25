2ecb381e3b5428dec779c35df0d819227ced2690
Joe Mulvaney <jmulvane@cshl.edu>
2016-02-24 22:37:26 -0500

    docs working -- change order of invocation in app.js

--- app.js
+++ app.js
@@ -19,19 +19,20 @@ var config = {
 SwaggerExpress.create(config, function (err, swaggerExpress) {
   if (err) { throw err; }
 
-  // install middleware
-  swaggerExpress.register(app);
-
-  var port = process.env.PORT || 10011;
-  app.listen(port);
-
-  console.log('Listening on', port);
-
-  // define routes
+  // define routes for documentation
   app.get('/', function (req, res, next) { // redirect to /docs with the correct schema
     res.redirect('/docs?url=/swagger');
   });
 
   app.use('/docs', express.static('node_modules/swagger-ui/dist'));
+
+  // install swagger server middleware
+  swaggerExpress.register(app);
+
+  // start it up
+  var port = process.env.PORT || 10011;
+  app.listen(port);
+
+  console.log('Listening on', port);
 });
 
