ec89b083424b7300f2d44a6dd7a44454defd7b9e
Andrew Olson <olson@cshl.edu>
2022-04-19 13:07:22 -0400

    updates for yeast

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ SwaggerExpress.create(config, function (err, swaggerExpress) {
 
   // start it up
   var version = +basePath.match(/\d+/);
-  var port = 20003; // process.env.PORT || 10000 + version;
+  var port = 60001; // process.env.PORT || 10000 + version;
   app.listen(port);
 
   console.log('Listening on', port);
