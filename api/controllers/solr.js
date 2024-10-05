29ef596e40e5e30f73e6bc733a0cce50dd496121
Andrew Olson <olson@cshl.edu>
2024-10-04 22:38:20 -0400

    atomic updates to saved_search field

--- api/controllers/solr.js
+++ api/controllers/solr.js
@@ -31,10 +31,43 @@ function solrRequest(req, res, streamMethod, additionalParams) {
   solrStream.pipe(res);
 }
 
-function validate(req, res) {
+async function validate(req, res) {
+  // get list of ids from request body
+  // uniqify and sort ids
+  // check if this set of ids has been validated before
+  // split list into manageable chunks
+  // validate the ids
+  // create a hash key for the validated ids
+  const solrURL = solrHelper.genesURL + '/select';
+
   var ids = req.body.ids;
   let uniqueIdentifiers = [...new Set(ids)].sort();
-  console.log(uniqueIdentifiers);
+
+  // check if this set of ids has been validated already
+  async function checkHash(hashed_input) {
+    try {
+      const params = {q:`saved_search:${hashed_input}`,rows:0};
+      const response = await axios.get(solrURL, {params});
+      // console.log(response.data.response.numFound, uniqueIdentifiers.length);
+      return response.data.response.numFound === uniqueIdentifiers.length;
+    } catch (error) {
+      console.error('error querying solr for saved_search:', error);
+    }
+  }
+  
+  const hashed_input = murmur.x86.hash32(uniqueIdentifiers.join(''));
+  const isSaved = await checkHash(hashed_input);
+
+  if (isSaved) {
+    // console.log("saved search found!");
+    res.json({
+      ids: uniqueIdentifiers,
+      missing: [],
+      hash: hashed_input
+    });
+    return;
+  }
+  // console.log("saved search not found");
 
   const chunkSize = 200;
 
@@ -50,16 +83,13 @@ function validate(req, res) {
   // Split the unique identifiers into chunks
   const identifierChunks = chunkArray(uniqueIdentifiers, chunkSize);
 
-
-  const solrURL = 'http://squam:8983/solr/sorghum_genes7/select';
-
   async function querySolr(chunk) {
     const params = {
       q: `id:(${chunk.join(' OR ')})`,
       fl: 'id',
       rows: chunk.length
     };
-    
+
     try {
       const response = await axios.get(solrURL, {params});
       return response.data.response.docs.map(doc => doc.id);
@@ -68,10 +98,10 @@ function validate(req, res) {
       return [];
     }
   }
- 
+
   async function checkIdentifiers() {
     let foundIds = [];
-  
+
     for (const chunk of identifierChunks) {
       const result = await querySolr(chunk);
       foundIds = foundIds.concat(result);
@@ -80,34 +110,41 @@ function validate(req, res) {
     // Compare the found IDs with the original list
     const missingIds = uniqueIdentifiers.filter(id => !foundIds.includes(id));
 
-    console.log('Found IDs:', foundIds);
-    console.log('Missing IDs:', missingIds);
-    
+    // build a hash for the found ids
+    const murmurhash = murmur.x86.hash32(foundIds.sort().join('')); // sorting because async
     res.json({
       ids: foundIds,
       missing: missingIds,
-      hash: murmur.x86.hash32(foundIds.join(''))
+      hash: murmurhash
+    });
+    // also do an atomic update to the gene docs
+    const updateData = foundIds.map(id => {
+      return {
+        id: id,
+        saved_search: { add: murmurhash }
+      }
+    });
+    // console.log("posting atomic updates", updateData);
+    axios.post(solrHelper.genesURL + '/update?commit=true', updateData, {
+      headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => {
+      // console.log('Update successful:', response.data)
+    })
+    .catch(error => {
+      console.error('Error while updating:', error);
     });
   }
-
   checkIdentifiers();
+}
 
-  // console.log(solrURL, params);
-  // axios.get(solrURL, { params })
-  // .then(response => {
-  //   const docs = response.data.response.docs;
-  //   console.log('docs:', docs);
-  //   const foundIds = response.data.response.docs.map(doc => doc.id);
-  //   console.log('Found IDs:', foundIds);
+function saveSearch(req, res) {
+  // given
+  // hash: 32bit integer
+  // name: string name for saved search
+  // public: boolean
+  // site: site where the saved search comes from
+  // authenticated user
   //
-  //   // Compare the found IDs with the original list
-  //   const missingIds = uniqueSortedIds.filter(id => !foundIds.includes(id));
-  //   console.log('Missing IDs:', missingIds);
-  //   res.json("done");
-  // })
-  // .catch(error => {
-  //   console.error('Error querying Solr:', error);
-  // });
-  //
-
+  // insert a record to mongodb collection
 }
